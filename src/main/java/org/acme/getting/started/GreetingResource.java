eb4864a84d593fd3e425be913136a0c976238b1a
Victor Ma <vma@redhat.com>
2021-07-13 14:25:46 -0400

    Add JFR code

--- src/main/java/org/acme/getting/started/GreetingResource.java
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -5,12 +5,59 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import jdk.jfr.*;
+
 @Path("/hello")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
+    public String hello() throws Exception {
+        Alpha a = new Alpha();
+        Beta b = new Beta();
+        Gamma g = new Gamma();
+
+        a.begin();
+        methodAlpha();
+        a.commit();
+
+        b.begin();
+        methodBeta();
+        b.commit();
+
+        g.begin();
+        methodGamma();
+        g.commit();
+
         return "Hello RESTEasy";
+    }
+
+    @Name("org.acme.getting.started.Alpha")
+    @Label("Alpha")
+    @Description("Event for the methodAlpha method")
+    static class Alpha extends Event {
+    }
+
+    @Name("org.acme.getting.started.Beta")
+    @Label("Beta")
+    @Description("Event for the methodBeta method")
+    static class Beta extends Event {
+    }
+
+    @Name("org.acme.getting.started.Gamma")
+    @Label("Gamma")
+    @Description("Event for the methodGamma method")
+    static class Gamma extends Event {
+    }
+
+    static void methodAlpha() throws InterruptedException {
+        Thread.sleep(500);
+    }
+
+    static void methodBeta() {
+    }
+
+    static void methodGamma() throws InterruptedException {
+        Thread.sleep(1000);
     }
 }
